3cbe6f8c3c790c2b2aba409de33368795a096ec4
mateusz.jarzebski <matjarzebski@gmail.com>
2024-07-09 22:32:56 +0200

    add

--- src/app/[viewport]/login/page.tsx
+++ src/app/[viewport]/login/page.tsx
@@ -1,6 +1,7 @@
 export const generateStaticParams = () => {
   return [{ viewport: 'd' }, { viewport: 'm' }]
 }
+export const dynamicParams = false
 
 const LoginPage = () => {
   return <p>{"I'm here! Yupi!"}</p>
